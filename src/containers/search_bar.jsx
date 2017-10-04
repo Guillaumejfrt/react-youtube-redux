04ae2ca36aa71fca5679ec11a461e3db81464336
Julien Da Silva <julien.da.silva@hotmail.com>
2017-10-04 15:15:11 +0200

    Youtube Api response

--- src/containers/search_bar.jsx
+++ src/containers/search_bar.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyDmb6Al9P1LrVQGctwbOzGmy7SS4emh2YI';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      word: ''
     };
   }
+  videoSearch = (word) => {
+    YTSearch({ key: API_KEY, term: word }, (videos) => {
+      console.log(videos);
+    });
+  }
   inputHandler = (event) => {
-    this.setState({ value: event.target.value });
+    const word = event.target.value;
+    this.setState({ word });
+    this.videoSearch(word);
   }
   render() {
     return (
       <div>
         <form className="form-group">
-          <input type="text" onChange={this.inputHandler} value={this.state.value} />
+          <input type="text" onChange={this.inputHandler} value={this.state.word} />
           <button className="btn btn-danger">Search</button>
         </form>
       </div>
