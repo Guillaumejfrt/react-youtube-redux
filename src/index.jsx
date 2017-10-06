1f82b710368393da452bb7b30ae74079f36ef185
Julien Da Silva <julien.da.silva@hotmail.com>
2017-10-06 17:13:14 +0200

    wow feature :)

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
-import App from './components/app';
+import Home from './components/home';
+import About from './components/about';
+import SelectedVideo from './containers/video_selected';
 import videosReducer from './reducers/videos_reducer';
 import selectedVideoReducer from './reducers/selected_video_reducer';
 import '../assets/stylesheets/application.scss';
@@ -28,9 +30,15 @@ const middlewares = applyMiddleware(reduxPromise, logger);
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers, initialState, middlewares)}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <div>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+        </Switch>
+      </Router>
+      <SelectedVideo /> 
+    </div>
   </Provider>,
   document.querySelector('.container')
 );
